a47ec4c4d05c5dcfe40372834cd6b49747457951
yashvant <yashvanttalawar6@gmail.com>
2025-12-06 10:00:57 +0530

    Update test_addition.py

--- test_addition.py
+++ test_addition.py
@@ -1,11 +1,11 @@
 from addition import add
 
 def test_add_positive_numbers():
-      assert add(2,3) --5
+      assert add(2,3) == 5
 
 def test_add_negative_numbers():
-  assert add (-4,-6) -- -10
+  assert add (-4,-6) == -10
 
 def test_add_zero():
-  assert add(0,5) --5
+  assert add(0,5) == 5
 
